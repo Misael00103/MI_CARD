1a53adee19ad95d1521c7176384c032630e6975f
Misael00103 <beriguetemisael@gmail.com>
2022-08-09 18:22:45 -0400

    first commit

---
+++ MI_CARD/MainPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Essentials;
+
+namespace MI_CARD
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void btnYoutube_Clicked(object sender, EventArgs e)
+        {
+            Browser.OpenAsync("https://www.youtube.com/channel/UCnps4nOdbsOhy4-cyBT_BFg", BrowserLaunchMode.SystemPreferred);
+        }
+
+        private void btnVerlinkedin_Clicked(object sender, EventArgs e)
+        {
+            Browser.OpenAsync("https://www.linkedin.com/in/misael-beriguete-b086a3222/", BrowserLaunchMode.SystemPreferred);
+        }
+
+        private void btnVerTwitter_Clicked(object sender, EventArgs e)
+        {
+            Browser.OpenAsync("https://twitter.com/BerigueteMisae1", BrowserLaunchMode.SystemPreferred);
+        }
+
+        private void btnVerInstagrams_Clicked(object sender, EventArgs e)
+        {
+            Browser.OpenAsync("https://www.instagram.com/misaeel_7/", BrowserLaunchMode.SystemPreferred);
+        }
+
+        private void btnVerFacebook_Clicked(object sender, EventArgs e)
+        {
+            Browser.OpenAsync("https://www.facebook.com/Misael2508/", BrowserLaunchMode.SystemPreferred);
+        }
+    }
+}
